48931d4d20207341fdca7bd4bb7fc66815c5ca9a
Mesut Çalım <mesutcalim.eee@gmail.com>
2022-03-11 19:09:50 +0300

    Ödev Tamamlandı

---
+++ Donguler/BolunenSayilar.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class BolunenSayilar {
+    public static void main(String[] args) {
+        int sayi,toplam=0,sayac=1;
+        double ort;
+        Scanner inp=new Scanner(System.in);
+        System.out.print("0'dan büyük bir sayı giriniz: ");
+        sayi= inp.nextInt();
+
+        for(int i=1;i<=sayi; i++){
+
+            if(i%12==0) {
+                toplam += i;
+                sayac++;
+            }
+        }
+        ort=toplam/(sayac-1);
+        System.out.println("3 ve 4 e bölünebilen sayıların ortalaması  "+ort);
+    }
+}
