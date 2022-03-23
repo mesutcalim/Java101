1772fbc03d0faa3ca0816036281cb12441431bea
Mesut Çalım <mesutcalim.eee@gmail.com>
2022-03-23 14:32:01 +0300

    Ödev Tamamlandı(İstenilen şekilde maça başlama oranı %50 olarak ayarlandı.)

---
+++ BoksOyunu/Main.java
@@ -0,0 +1,10 @@
+package BoksOyunu;
+
+public class Main {
+    public static void main(String[] args) {
+        Fighter marc = new Fighter("Marc" , 10 , 100, 90, 15);
+        Fighter alex = new Fighter("Alex" , 10 , 100, 100, 15);
+        Ring r = new Ring(marc,alex , 90 , 100);
+        r.run();
+    }
+}
